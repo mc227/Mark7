878e5610e28ee614512469e694b380d83c91ef86
mc227 <markcostales.mark@gmail.com>
2022-05-13 21:50:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://github.com/mc227'
+            // solution
+        }
+    }
+})
