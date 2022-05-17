ecd974cf15aedb33aaa3d9c785259306c6107f5d
mc227 <markcostales.mark@gmail.com>
2022-05-17 18:52:56 -0400

    before event handling

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            details:['50% cotton', '30% wool', '20% polyester'],
+            variants: [{id: 2234, color:'green'},{id:2235, color:'blue'},],
         }
     }
 })
